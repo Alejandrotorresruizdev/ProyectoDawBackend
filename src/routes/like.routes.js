4b954f87449c412e3cd4e39ecd6a27b8bfad327e
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-02 16:10:23 +0200

    [ADD] Get all likes from post

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -4,6 +4,7 @@ module.exports = function({LikeController}) {
     const router = Router();
 
     router.post('', LikeController.create);
+    router.get('/:id/all',LikeController.getAllLikesFromPost)
 
     return router;
 }
--- src/services/like.service.js
+++ src/services/like.service.js
@@ -1,11 +1,40 @@
 const BaseService = require("./base.service");
 
-class LikeService extends BaseService  {
-  constructor({LikeRepository}) {
-    super(LikeRepository);
-  }
-  
+let _likeRepository = null;
 
+const responseFunctions = require("../utils/responseHttp.utils");
+
+const {
+  CODE_NOT_FOUND,
+  CODE_OK
+} = require("../constants/httpCodes");
+
+const {
+  MESS_EMPTY_ID,
+  MESS_ID_NOT_FOUND,
+  MESS_OK_GET,
+} = require("../constants/errorMessages");
+
+class LikeService extends BaseService {
+  constructor({ LikeRepository }) {
+    super(LikeRepository);
+    _likeRepository = LikeRepository;
+  }
+
+  async getAllLikesFromPost(id) {
+    if (responseFunctions.emptyId(id)) {
+      return responseFunctions.error(CODE_NOT_FOUND, MESS_EMPTY_ID);
+    }
+
+    const allEntities = await _likeRepository.getAllLikesFromPost(id);
+
+
+    if (responseFunctions.notFoundEntity(allEntities)) {
+      return responseFunctions.error(CODE_NOT_FOUND, MESS_ID_NOT_FOUND);
+    }
+
+    return responseFunctions.error(CODE_OK, MESS_OK_GET, allEntities);
+  }
 }
 
 module.exports = LikeService;
