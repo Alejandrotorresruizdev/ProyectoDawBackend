fdc8c949362e728267c6cb0206fc44c072a181e8
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-13 23:37:29 +0200

    make it better

--- src/utils/dbSetup.utils.js
+++ src/utils/dbSetup.utils.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
-const container = require('../startup/container');
 const {MYSQL_URI,MYSQL_DB,MYSQL_USER,MYSQL_PASS} = require('.');
 
-const sequelize = new Sequelize(MYSQL_DB,MYSQL_USER, MYSQL_PASS, {
-    host: MYSQL_URI,
+const sequelize = new Sequelize("heroku_ff327546e53f9a3","b995a4247fce61","89c8710b", {
+    host:"eu-cdbr-west-02.cleardb.net",
     dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
     logging: false
   });
